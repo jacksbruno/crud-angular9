70798efd32ccae1722426221b2ea90299efa1e0c
jacksbruno <jacksbruno22@gmail.com>
2021-10-02 07:40:42 -0400

    Iniciando salvar de produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import '@angular/compiler';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,12 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
-import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +28,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +39,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatToolbarModule,
     MatCardModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,25 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.scss']
+})
+export class ProductCreateComponent implements OnInit {
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto salvo!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
